495f1df169fc7bfa2be41f5c0763bf7c7a16603b
Chiano <bigchiano@protonmail.com>
2025-04-16 06:20:59 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DATA_DIR=~/data
+
+wget https://storage.googleapis.com/golang/go1.19.linux-amd64.tar.gz
+tar -xvf go1.19.linux-amd64.tar.gz
+sudo rm -fr /usr/local/go
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+cd ~
+git clone https://github.com/safuchain/go-ethereum
+cd go-ethereum/
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo apt-get install build-essential make git screen unzip curl nginx pkg-config nmap xterm screen tcl -y
+make geth
+
+./build/bin/geth --datadir $DATA_DIR init ./genesis.json
